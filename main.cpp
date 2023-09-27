57a5dd959d7a0f5d743052182a40317eaedd0571
Matheus Oliveira <w0471251@nscc.ca>
2023-09-27 15:09:52 -0300

    more changes

--- main.cpp
+++ main.cpp
@@ -16,41 +16,41 @@ int main() {
     string fileName;
 
     do {
-        // ask for the filePath
+        // Ask for the filePath
         //SOURCE: https://stackoverflow.com/questions/70924991/check-if-directory-exists-using-filesystem
-        cout << "Please insert a valid file path to save your file: \nExample: (C:\\Users\\[USER NAME]\\Desktop\\):" << endl;
+        cout << "\nPlease insert a valid file path to save your file: \nExample: (C:\\Users\\[USER NAME]\\Desktop\\):" << endl;
         getline(cin, filePath);
         bool filepathExists = std::filesystem::is_directory(filePath);
 
+        // If the file path exists this will include a / if the user miss it
         if (filepathExists) {
-            cout << "Your file path is GOOD: " << filePath << endl;
+            cout << "\nYour file path is GOOD: " << filePath << endl;
             if (filePath.back() != '/' && filePath.back() != '\\') {
                 filePath += '/';
             }
             isValidFilePath = true;
 
         } else {
-            cout << "Sorry, it is an invalid file path.\nPlease try it again.\n" << endl;
+            cout << "\nSorry, it is an invalid file path.\nPlease try it again.\n" << endl;
         }
 
     } while (!isValidFilePath);
 
-    // regex source: https://www.experts-exchange.com/questions/22779026/Regex-for-a-valid-Windows-file-name.html
+    // Regex source: https://www.experts-exchange.com/questions/22779026/Regex-for-a-valid-Windows-file-name.html
     regex reg("^(?!((con|prn|aux)((\\.[^\\\\/:\"*?<>|]{1,3}$)|$))|[\\s\\.])[^\\\\/:*?<>|]{1,254}$");
 
     do {
-
-        // ask for the fileName
-        cout << "Please insert your file name: " << endl;
+        // Ask for the fileName
+        cout << "\nPlease insert your file name: \nExample: (myFile):" << endl;
         getline(cin, fileName);
         if (regex_search(fileName, reg)) {
-            cout << "It is a valid name, thank you!" << endl;
+            cout << "\nIt is a valid name, thank you!" << endl;
             isValidFileName = true;
         } else {
-            cout << "The name is invalid, please insert it again." << endl;
+            cout << "\nThe name is invalid, please insert it again." << endl;
         }
 
-        cout << "Your file name is: " << fileName << ".html" << endl;
+        cout << "Your full file name is: " << fileName << ".html" << endl;
 
     } while (!isValidFileName);
 
@@ -60,21 +60,19 @@ int main() {
     std::ofstream outputFile(filePath + fileName + ".html", std::ios::out);
 
     // SOURCE: https://www.udacity.com/blog/2021/05/how-to-read-from-a-file-in-cpp.html
-    if(inputFile.is_open()) {
-        char mychar;
-        outputFile << "<PRE>" << std::endl;
+    if(!inputFile.fail() && !outputFile.fail()) {
+        char mychar = inputFile.get(); // gets the first char
 
-        while(inputFile) {
-            mychar = inputFile.get();
-            if (inputFile.eof()) { // avoid the end of file character
-                break;
-            } else if (mychar == '<') {
+        outputFile << "<PRE>" << std::endl;
+        while(!inputFile.eof()) {
+            if (mychar == '<') {
                 outputFile << "&lt;";
             } else if (mychar == '>') {
                 outputFile << "&gt;";
             } else {
                 outputFile << mychar;
             }
+            mychar = inputFile.get(); // gets the next char before loop it again
         }
         outputFile << "</PRE>" << std::endl;
 
